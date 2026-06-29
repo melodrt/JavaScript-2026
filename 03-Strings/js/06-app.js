5de5435fcf90b3daf6e684557eccb44ae4321509
meloduarte <devscustomers@gmail.com>
2026-06-29 10:42:50 -0500

    06-app.js string

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,14 @@
+const producto = "Monitor 20 Pulgadas";
+const texto = ' en promocion'.repeat(3);
+
+console.log(texto);
+console.log(`${producto}${texto}`);
+
+const actividad = "Estoy aprendiendo JavaScript Moderno";
+console.log(actividad.split(" "));
+
+const hobbies = "Leer, caminar, escuchar música, escribir, aprender a programar";
+console.log(hobbies.split(", "));
+
+const tweet = "Aprendiendo JavaScript Moderno #MeloDev.PRO";
+console.log(tweet.split("#"));
