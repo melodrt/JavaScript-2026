16736d9a3506df4573b52358e1bb04bfb1d482a9
meloduarte <devscustomers@gmail.com>
2026-06-29 10:56:46 -0500

    01-app.js numeros

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,14 @@
+// Creacion de numeros
+const numero1 = 30;
+const numero2 = 20;
+const numero3 = 20.20;
+const numero4 = -3;
+const numero5 = .1087564;
+const numero6 = new Number(80); // No es recomendable usarlo
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+console.log(numero6);
