689d35cf58a7e5610f93d211a928755cd4437775
meloduarte <devscustomers@gmail.com>
2026-06-29 11:02:24 -0500

    02-app.js numeros

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,27 @@
+const numero1 = 37;
+const numero2 = 25;
+
+let resultado;
+console.log(resultado);
+
+// suma
+resultado = numero1 + numero2;
+console.log(`suma: ${resultado}`);
+
+// resta
+resultado = numero1 - numero2;
+console.log(`resta: ${resultado}`);
+
+// multiplicacion
+resultado = numero1 * numero2;
+console.log(`multiplicacion: ${resultado}`);
+
+// division
+resultado = numero1 / numero2;
+console.log(`division: ${resultado}`);
+
+// modulo
+resultado = numero1 % numero2;
+console.log(`modulo: ${resultado}`);
+
+
