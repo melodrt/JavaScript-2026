343ea0de6998ff2d46ff107e434c60a5c51e1d32
meloduarte <devscustomers@gmail.com>
2026-06-29 10:52:29 -0500

    intro, var, string

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,10 @@
+const producto = "Monitor 20 Pulgadas";
+console.log(producto.toUpperCase()); // Convierte a mayúsculas
+console.log(producto.toLowerCase()); // Convierte a minúsculas
+
+const email = "CORREO@CORREO.COM";
+console.log(email.toLowerCase()); // Convierte a minúsculas
+
+const precio = 300;
+console.log(precio); // Muestra el número
+console.log(precio.toString()); // Convierte a string
